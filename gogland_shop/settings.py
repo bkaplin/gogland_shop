0f680261ea21c7ba3d08b032fd58a556db24d908
b.kaplin <b,kaplin@agora.ru>
2022-04-02 23:01:56 +0300

    settings

--- gogland_shop/settings.py
+++ gogland_shop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eum_*vhtm5fkgze9yqqufl7ns_ecj!-y1(c4ts1u(*ffdfa57-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
