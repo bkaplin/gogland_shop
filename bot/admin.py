218aadf5933c868021aaf357a6a2906873fcd6d2
b.kaplin <b,kaplin@agora.ru>
2022-06-03 14:58:11 +0300

    Добавил модель и все что нужно для отправки групповых сообщений от бота. добавил миграции

--- bot/admin.py
+++ bot/admin.py
@@ -1,8 +1,55 @@
 from django.contrib import admin
 
-from bot.models import Chat, CardNumber, ShopSettings
+from bot.models import Chat, CardNumber, ShopSettings, GroupBotMessage
+from django.utils.translation import gettext as _, gettext_lazy as l_
+
+
+class GroupBotMessageAdmin(admin.ModelAdmin):
+    EMPTY_LABEL = '-'
+
+    list_display = [
+        'id',
+        'message_text',
+        'sent',
+
+        'created_at',
+        'updated_at',
+        'sent_at',
+    ]
+
+    list_display_links = [
+        'id',
+        'message_text',
+    ]
+
+    readonly_fields = [
+        'log',
+        'created_at',
+        'updated_at',
+        'sent_at',
+        'sent',
+    ]
+
+    search_fields = [
+        'message_text',
+    ]
+
+    list_filter = [
+        'sent',
+    ]
+
+    def send(self, request, queryset):
+        for item in queryset:
+            item.send()
+    send.short_description = _(u'Отправить')
+
+    actions = [
+        'send',
+    ]
+
 
 admin.site.register(Chat)
 admin.site.register(CardNumber)
 admin.site.register(ShopSettings)
+admin.site.register(GroupBotMessage, GroupBotMessageAdmin)
 
--- bot/models.py
+++ bot/models.py
@@ -1,8 +1,13 @@
+import datetime
+
+from django.conf import settings
 from django.db import models
 from django.db.models import SET_NULL
+from django.utils import timezone
 from django.utils.translation import gettext as _, gettext_lazy as l_
 
 from user.models import User
+import telegram
 
 
 class Chat(models.Model):
@@ -28,6 +33,45 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.chat.user}: {self.text}' if self.chat and self.chat.user else str(self.pk)
+
+
+class GroupBotMessage(models.Model):
+    created_at = models.DateTimeField(verbose_name=l_(u'Дата создания'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=l_(u'Дата обновления'), auto_now_add=True)
+    sent_at = models.DateTimeField(verbose_name=l_(u'Дата последней отправки'), blank=True, null=True)
+
+    users = models.ManyToManyField(User, verbose_name=l_(u'Пользователи для отправки сообщения'), related_name='group_messages')
+    message_text = models.TextField(verbose_name=l_(u'Текст сообщения'), default='')
+    sent = models.BooleanField(verbose_name=l_(u'Отправлено'), default=False)
+    log = models.TextField(verbose_name=l_(u'Лог отправки'), default='', blank=True, null=True)
+
+    class Meta:
+        verbose_name = l_(u'Групповое сообщение от бота')
+        verbose_name_plural = l_(u'Групповые сообщения от бота')
+
+    def __str__(self):
+        return self.message_text
+
+    def send(self):
+        bot = telegram.Bot(token=settings.TG_TOKEN)
+        now = datetime.datetime.now()
+        self.sent_at = now
+        self.sent = True
+
+        log_item = '{}: {}\t{}\n'
+
+        self.log += f'{now.strftime("%d.%m.%Y %H:%M:%S")}\n'
+
+        for user in self.users.all():
+            try:
+                bot.send_message(text=self.message_text, chat_id=user.tg_id)
+                status = 'OK'
+            except:
+                status = 'ERROR'
+            self.log += log_item.format(user.tg_id, user.full_name, status)
+        self.log += '\n\n'
+
+        self.save()
 
 
 class CardNumber(models.Model):
